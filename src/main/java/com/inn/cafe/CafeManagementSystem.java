031529cdf1a38efe61796079be61a2c40f79d8ed
devboy2011 <buituananhit.contact@gmail.com>
2023-09-02 10:13:09 +0700

    Signup API of User & Structure

---
+++ src/main/java/com/inn/cafe/CafeManagementSystem.java
@@ -0,0 +1,14 @@
+package com.inn.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystem {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CafeManagementSystem.class, args);
+	}
+
+}
